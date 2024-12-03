0b207f1ee83f53726c9abc55d0be39123e559d01
Hiram Fleitas <hiramfleitas@hotmail.com>
2024-12-03 13:21:07 -0500

    Update EnableCDC.sql

--- EnableCDC.sql
+++ EnableCDC.sql
@@ -10,6 +10,11 @@ exec sys.sp_cdc_enable_table
     @source_name = 'Customer',
     @role_name=NULL --control access to change data.
 go
+
+/* 2. What about heaps? */
+create table mytable (col1 varchar(max), col2 datetime)
+go
+
 exec sys.sp_cdc_enable_table 
     @source_schema = 'dbo',
     @source_name = 'mytable',
@@ -19,11 +24,9 @@ go
 select name, is_cdc_enabled from sys.databases
 select name, is_tracked_by_cdc from sys.tables --Fabric RTI CDC connector does NOT require enabling per table.
 go
-
-/* 2. What about heaps? */
-create table mytable (col1 varchar(max), col2 datetime)
-go
-
+select * from cdc.cdc_jobs --cleanup retention 4320minutes = 3days
+-- exec sys.sp_cdc_change_job @name='cleanup', @retention=60 --if need to change it.
 
 /* 3. Eventstream Azure SQL DB CDC data source for tables: SalesLT.Customer, dbo.mytable */ 
 -- https://app.powerbi.com/workloads/oneriver/hub/NewSources?experience=kusto
+-- retention & throughput: https://learn.microsoft.com/en-us/fabric/real-time-intelligence/event-streams/configure-settings
